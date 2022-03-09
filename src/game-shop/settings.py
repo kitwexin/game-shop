52a13a62159de76436f654d0ab37c94d6f7d40cc
Aleksey <kitwexin@gmail.com>
2022-03-09 13:59:48 +0300

    game model created

--- src/game-shop/settings.py
+++ src/game-shop/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'games',
+    'companies',
+    'genres'
 ]
 
 MIDDLEWARE = [
@@ -123,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
---
+++ src/games/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.forms import DateTimeField
+from companies.models import Company
+from genres.models import Genre
+
+class Game(models.Model):
+    name = models.CharField(
+        verbose_name = 'Game name',
+        max_length = 50,
+    )
+    price = models.IntegerField(
+        verbose_name = 'Game price USD',
+        default = 0,
+    )
+    release_date = models.DateField(
+        verbose_name = 'Release date',
+        null=True,
+        blank=True
+    )
+    development_company = models.ForeignKey(
+        Company,
+        on_delete = models.CASCADE,
+        verbose_name = 'Development company',
+        default = 'Unknown'
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete = models.CASCADE,
+        verbose_name = 'Game genre',
+        default = 'No genre'
+    )
+    image = models.ImageField(
+        verbose_name = 'Game cover',
+        upload_to = 'games-pics',
+        null=True,
+        blank=True
+    )
+    description = models.TextField(
+        verbose_name = 'Game description',
+    )
+    age_limit = models.IntegerField(
+        verbose_name = 'Age limit',
+        default = 0
+    )
+
+    def __str__(self):
+        return self.name
